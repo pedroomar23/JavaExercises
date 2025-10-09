822c04059184dfeeb4c8e6d75a304a62d9bbf76b
Pedro Omar <pocl9812@gmail.com>
2025-10-09 12:00:30 -0400

    new: exercise 1

--- Ejercicio1/ejercicio1.java
+++ Ejercicio1/ejercicio1.java
@@ -1,23 +1,9 @@
-int numero = 1;
-String nombre = "Pedro";
-Boolean test = false;
+class Test {
+    String nombre = "Pedro";
+    int numero = 5;
 
-if (number > 1) {
-    Sout("Esto esta incorrecto");
-} else {
-    Sout("Esto esta incorrecto");
-}
-
-public class Test {
-    String nombre = "";
-    int numero = 2;
-    int numero1 = 3;
-
-    this.nombre = nombre;
-    this.numero = numero;
-    this.numero1 = numero1;
-
-    public void suma(numero + numero1) {
-        return numero + numero1
+    public Test(String nombre, int numero) {
+        this.nombre = nombre;
+        this.numero = numero;
     }
 }
---
+++ Ejercicio2/ejercicio2.java
@@ -0,0 +1,11 @@
+class NewTest {
+    String nombre = "Carlos";
+    int numero = 5;
+    boolean isValid = false;
+
+    public NewTest(String nombre, int numero, boolean isValid) {
+        this.nombre = nombre;
+        this.numero = numero;
+        this.isValid = isValid;
+    }
+}
